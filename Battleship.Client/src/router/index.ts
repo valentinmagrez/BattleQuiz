5fee1f7ec47236e52db92387721e1db0e3bc68ad
valentinmagrez <valentin.magrez@gmail.com>
2020-05-03 17:09:56 +0200

    remove example routing

--- Battleship.Client/src/router/index.ts
+++ Battleship.Client/src/router/index.ts
@@ -11,16 +11,11 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: loginRoute.path,
+    name: loginRoute.name,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: loginRoute.path,
-    name: loginRoute.name,
     component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   }
 ]
